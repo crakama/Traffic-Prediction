b3619285f2a898bd09c55220a409cf13a4a86cfc
crakama <crakama89@gmail.com>
2018-11-07 08:27:34 +0100

    Read files and record time

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "DataMining"
+
+version := "0.1"
+
+scalaVersion := "2.12.7"
+//To use Loan Pattern when working with files is to use Joshua Suereth’s ARM library
+// for automatically closing the file
+
+libraryDependencies += "com.jsuereth" %% "scala-arm" % "1.3"
--- src/main/scala/Shingling.scala
+++ src/main/scala/Shingling.scala
@@ -1,3 +1,39 @@
-class Shingling {
+
+import Control._
+
+
+object Shingling {
+  var noofDocs = 0
+  def main(args: Array[String]): Unit = {
+    var dataset = " "
+    val documentsCorpus = new java.io.File("data").listFiles.filter(_.getName.endsWith(".txt"))
+    timed( for (doc <- documentsCorpus) {
+      using(io.Source.fromFile(doc)) { source => {
+        println(doc)
+        dataset = dataset + source
+        noofDocs = noofDocs +1
+      }}})
+
+
+    print ("Transforming data...")
+
+  }
+
+
+  //=============================================================================
+  //               Convert Documents To Sets of Shingles of k-sizes
+  // =============================================================================
+
+  //print("Please Enter lenght of K-Shingles:")
+  //val kGram = Console.readLine()
+
+  // measures time taken by a block of code
+  def timed[A](block: => A) = {
+    val t0 = System.currentTimeMillis
+    val result = block
+
+    println(" Reading " + noofDocs + " documents took " + (System.currentTimeMillis - t0) + " ms")
+    result
+  }
 
 }
