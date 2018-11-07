f3bc98c17ad2bcc3764d3fd0bfa53c53fb4bf314
crakama <crakama89@gmail.com>
2018-11-07 13:34:11 +0100

    Remove unwanted special characters from documents

--- src/main/scala/Shingling.scala
+++ src/main/scala/Shingling.scala
@@ -1,24 +1,23 @@
-
+import org.jsoup.Jsoup
+import org.jsoup.nodes.{Document, Element}
 import Control._
-
 
 object Shingling {
   var noofDocs = 0
   def main(args: Array[String]): Unit = {
     var dataset = " "
-    val documentsCorpus = new java.io.File("data").listFiles.filter(_.getName.endsWith(".txt"))
+    val documentsCorpus = new java.io.File("data").listFiles.filter(_.getName.endsWith(".sgm"))
     timed( for (doc <- documentsCorpus) {
-      using(io.Source.fromFile(doc)) { source => {
-        println(doc)
-        dataset = dataset + source
+      using(io.Source.fromFile(doc,"iso-8859-1")) { source => {
+       // println(source.mkString)
+        dataset = dataset + source.mkString
         noofDocs = noofDocs +1
       }}})
 
-
     print ("Transforming data...")
+    getAllLinks(dataset)
 
   }
-
 
   //=============================================================================
   //               Convert Documents To Sets of Shingles of k-sizes
@@ -34,6 +33,17 @@ object Shingling {
 
     println(" Reading " + noofDocs + " documents took " + (System.currentTimeMillis - t0) + " ms")
     result
+  }
+
+  def getAllLinks(content: String) = {
+    ////println(content)
+    var documents = List[String]()
+    val doc: Document = Jsoup.parse(content, "iso-8859-1")
+    val bodyWords = doc.select("body").first().text()
+    val replace: String = bodyWords.replaceAll("[^\\w\\s]", " ").toLowerCase()
+      //val replace = bodyWords.replaceAll("[\\.$|,|;|:|'|=|\\|^\\\"|\\\"$ |\\[ |\\]|}|\\( \\) ]", " ").toLowerCase
+      documents = replace :: documents
+    println(documents)
   }
 
 }
